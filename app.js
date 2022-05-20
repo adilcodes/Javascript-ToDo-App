fb40ced010c25551be7b4caa37369120876477d3
Adil <ascodesofficial@gmail.com>
2022-05-21 03:30:32 +0500

    Added the functionality of form validation and collecting data from the user via form

--- app.js
+++ app.js
@@ -5,3 +5,33 @@ let textArea = document.getElementById("textarea");
 let msg = document.getElementById("msg");
 let tasks = document.getElementById("tasks");
 let addBtn = document.getElementById("addBtn");
+
+// Form Validation
+let formValidation = () => {
+    if(taskInput.value === ""){
+        console.log("failure");
+        msg.innerHTML = "Task cannot be blank";
+    }else{
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
+});
+
+// Data Collection
+
+let data = [];
+let acceptData = () => {
+    data.push({
+        task: taskInput.value,
+        date: dateInput.value,
+        textArea: textArea.value,
+    });
+    console.log(data);
+}
+
