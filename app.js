47491978f7f78848493e552ea10b8b1de2a0db92
Adil <ascodesofficial@gmail.com>
2022-05-20 21:44:03 +0500

    Starting the Javascript code

---
+++ app.js
@@ -0,0 +1,7 @@
+let form = document.getElementById("form");
+let taskInput = document.getElementById("taskInput");
+let dateInput = document.getElementById("dateInput");
+let textArea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let addBtn = document.getElementById("addBtn");
